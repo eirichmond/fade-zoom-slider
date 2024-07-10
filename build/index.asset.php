e80280214b388caf23f40f0594e564a55585b60a
Elliott Richmond <elliott@squareonemd.co.uk>
2024-07-10 06:55:16 +0100

    button further update

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '035704b025904f098968');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5ba7e68bbf8ce3906dae');
